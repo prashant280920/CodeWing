5ff16234b4a6bbffd9c0b5dad39325b641a7f5f7
prashant280920 <b19101@students.iitmandi.ac.in>
2022-05-20 23:01:24 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,104 @@
+<script>
+	import Message from "./components/Chat.svelte"; 
+	import Header from "./components/Header.svelte";
+	import SideNav from "./components/SideNav.svelte";
+	import CodeEditor from "./components/CodeEditor.svelte";
+	import Board from "./components/Board.svelte";
+	let state = {
+	  color: "#000000",
+			  size: "5"
+	};
+  
+	function changeColor(params) {
+		  console.log(params.target.value);
+			  state.color = params.target.value
+			  console.log(state.color);
+			}
+  
+	  function changeSize(params) {
+		  console.log(params.target.value);
+		  state.size =  params.target.value
+	  }
+  
+  </script>
+  
+  <Header imageSrc="images/ShareCode.png" />
+  
+  <div class="wrapper">
+  
+	  <CodeEditor />
+	<SideNav />
+	<Message/> 
+	
+	
+  </div>
+  
+	 
+  <div class="container">
+	<div class="tools-section">
+		<!-- <div class="color-picker-container">
+			Select Brush Color : &nbsp; 
+			<input type="color" value={state.color} on:change={event => changeColor(event)}/>
+		</div>
+  
+		<div class="brushsize-container">
+			Select Brush Size : &nbsp; 
+			<select value={state.size} on:change={event => changeSize(event)}>
+				<option> 5 </option>
+				<option> 10 </option>
+				<option> 15 </option>
+				<option> 20 </option>
+				<option> 25 </option>
+				<option> 30 </option>
+			</select>
+		</div> -->
+		WhiteBoard
+  
+	</div>
+  
+	<div class="board-container">
+		<Board color={state.color} size={state.size}></Board>
+	</div>
+  </div> 
+  
+  <!-- <style>
+	
+  </style> -->
+  
+  
+  <style>
+	.wrapper {
+	  display: flex;
+	  height: 65%;
+	}
+	.container {
+	  position: fixed;
+	  width: 40%;
+	  height: 100%;
+	  background: linear-gradient(45deg, #03A9F4, #4CAF50);
+  }
+  
+  .color-picker-container {
+	  text-align: center;
+	  display: inline;
+	  color: white;
+  }
+  
+  .brushsize-container {
+	  display: inline;
+	  color: white;
+	  margin-left: 12px;
+  }
+  
+  .tools-section {
+	  text-align: center;
+  }
+  
+	.board-container {
+		width: 100%;
+		height: 40%;
+		margin: auto;
+		margin-top: 1%;
+		background: white;
+	}
+  </style>
---
+++ src/components/Board.svelte
@@ -0,0 +1,105 @@
+<script>
+    import io from 'socket.io-client';
+    export let color;
+    export let size;
+    // export let state;
+    // console.log(size);
+    import { onMount } from "svelte";
+    var timeout;
+    var socket = io.connect("http://localhost:2000");
+    var ctx;
+    var isDrawing = false;
+    
+    onMount(() => {
+        drawOnCanvas();
+    });
+    socket.on("canvas-data", function(data){
+
+        var root = this;
+        var interval = setInterval(function(){
+            if(root.isDrawing) return;
+            root.isDrawing = true;
+            clearInterval(interval);
+            var image = new Image();
+            var canvas = document.querySelector('#board');
+            var ctx = canvas.getContext('2d');
+            image.onload = function() {
+                ctx.drawImage(image, 0, 0);
+
+                root.isDrawing = false;
+            };
+            image.src = data;
+        }, 200)
+    })
+    const drawOnCanvas = () => {
+        var canvas = document.querySelector('#board');
+        ctx = canvas.getContext('2d');
+        // var ctx = ctx;
+
+        var sketch = document.querySelector('#sketch');
+        var sketch_style = getComputedStyle(sketch);
+        canvas.width = parseInt(sketch_style.getPropertyValue('width'));
+        canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+
+        var mouse = {x: 0, y: 0};
+        var last_mouse = {x: 0, y: 0};
+
+        /* Mouse Capturing Work */
+        canvas.addEventListener('mousemove', function(e) {
+            last_mouse.x = mouse.x;
+            last_mouse.y = mouse.y;
+            // console.log(mouse.x, last_mouse.x, e.pageX, this.offsetLeft, canvas.width)
+            mouse.x = (e.pageX - this.offsetLeft) ;
+            mouse.y = (e.pageY - this.offsetTop-600);
+        }, false);
+
+
+        /* Drawing on Paint App */
+        // console.log(size);
+        ctx.lineWidth = size;
+        ctx.lineJoin = 'round';
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = color;
+
+        canvas.addEventListener('mousedown', function(e) {
+            canvas.addEventListener('mousemove', onPaint, false);
+        }, false);
+
+        canvas.addEventListener('mouseup', function() {
+            canvas.removeEventListener('mousemove', onPaint, false);
+        }, false);
+
+        var root = this;
+        console.log(root);
+        var onPaint = function() {
+            ctx.beginPath();
+            ctx.moveTo(last_mouse.x, last_mouse.y);
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.closePath();
+            ctx.stroke();
+
+            
+            var base64ImageData = canvas.toDataURL("image/png");
+            socket.emit("canvas-data", base64ImageData);
+        };
+    }
+
+</script>
+
+
+<div class="sketch" id="sketch">
+    <canvas class="board" id="board"></canvas>
+</div>
+
+
+<style>
+    .board {
+    width: 100%;
+    height: 100%;
+}
+
+.sketch {
+    width: 100%;
+    height: 100%;
+}
+</style>
